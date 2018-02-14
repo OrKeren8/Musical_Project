492e676a87e75e69e415dad57f2f62e5dca1106d
OrKeren8 <35062001+OrKeren8@users.noreply.github.com>
2018-02-14 11:56:48 +0200

    Add files via upload

--- python_XML_parsing/xmlParsingTest.py
+++ python_XML_parsing/xmlParsingTest.py
@@ -15,27 +15,41 @@ file.close()
 nfile.close()
 
 dom = ET.parse(file_name)
-####
+
 notes = dom.findall('score-partwise/part/measure/note')
 translate = {'16th' : 1/16,'eighth' : 1/8,'quarter' : 1/4, 'half' : 1/2, 'full' : 1.0}
 inverseTranslate = {1/16 : '16th', 1/8 : 'eighth', 1/4 : 'quarter', 1/2 : 'half', 1.0 : 'full'}
 refrence = -1
 
+#Finds Refrence of duration 1 - i.e. base tempo
 for note in notes:
-    if note.find('duration').text == '1' and note.find('rest') == None:
-        refrence = translate[note.find('type').text]
+    try:
+        if note.find('duration').text == '1':
+            refrence = translate[note.find('type').text]
+            break
+    except:
+        refrence = 1
         break
-
+#Loop through all the notes
 for note in notes:
+    length = int(note.find('duration').text) * (refrence)
+    #find if a length of a note is too big i.e. more then one measure
+    if not length in inverseTranslate.keys():
+        key_list = list(inverseTranslate.keys())
+        for i in range(len(key_list)):
+            if not length > key_list[i]:
+                length = key_list[i-1]
+                break
+            
+    #If the current note is not a rest, print its pitch, and octave
     if note.find('rest') == None:
         step = note.find('pitch/step').text
         octave = note.find('pitch/octave').text
-        length = int(note.find('duration').text) * refrence
         print('{0} {1}  - {2}'.format(step, octave, inverseTranslate[length]))
+    #Otherwise, print the rest with its duration
     else:
-        length = int(note.find('duration').text) * refrence
         print('rest - {0}'.format(inverseTranslate[length])) 
-
+        pass
 print('success!')
 
 
